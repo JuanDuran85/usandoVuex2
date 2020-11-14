040617bc8b60e1f34076fe939f871f1db537d090
JuanDuran85 <duran85juan@gmail.com>
2020-11-14 18:42:46 -0300

    propiedaddes computadas con get y set

--- src/views/Inicio.vue
+++ src/views/Inicio.vue
@@ -6,6 +6,14 @@
     <div class="alert alert-info" role="alert">
       {{enviandoMsg2}}
     </div>
+    <h2 class="mt-4">Usando propiedades computadas con get y set</h2>
+    <div class="alert alert-info mt-3" role="alert">
+      {{nombreCompleto}}
+    </div>
+    <div class="form-group">
+      <label for="">Modificar Nombre Completo</label>
+      <input type="text" class="form-control" placeholder="Nombre y Apellido" v-model="nombreCompleto">
+    </div>
   </div>
 </template>
 
@@ -14,8 +22,26 @@ import { mapGetters } from "vuex";
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      nombre: 'Pedro',
+      apellido: 'Perez'
+    }
+  },
   computed: {
-    ...mapGetters(['enviandoMsg1','enviandoMsg2'])
+    ...mapGetters(['enviandoMsg1','enviandoMsg2']),
+    nombreCompleto: {
+      get(){
+        let valor = this.nombre;
+        valor += this.apellido ? ` ${this.apellido}` : ''
+        return valor;
+      },
+      set(valor){
+        let nombres = valor.split(' ');
+        this.nombre = nombres[0];
+        this.apellido = nombres.length === 1 ? '' : nombres[nombres.length-1];
+      }
+    }
 /*     traerMsg1(){
       return this.$store.getters.enviandoMsg1;
     },
